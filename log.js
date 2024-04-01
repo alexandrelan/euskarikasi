e8f4f71dffee16c17bbc41a6438e38876a6e227d
alexandrelan <alexandrelan@users.noreply.github.com>
2024-04-01 14:31:48 +0200

    logger

--- log.js
+++ log.js
@@ -1,8 +1,21 @@
+function createLogger() {
+  const logger = document.createElement("div");
+  logger.style.position = 'fixed';
+  logger.style.bottom = '0';
+  logger.height = '25%';
+  document.body.appendChild(logger);
+}
+
 function logUnderFingerElement(event) {
-console.log(event.type)
-  console.log(event.target);
-  console.log(event.currentTarget);
-  console.log('---');
+  const logger = document.getElementById("logger");
+  if (!logger) {
+    createLogger();
+    logUnderFingerElement(event);
+  } else {
+    console.log( event.type)
+    console.log(event.target);
+    console.log('---');
+  }
 }
 
 document.addEventListener('mousemove', logUnderFingerElement);
