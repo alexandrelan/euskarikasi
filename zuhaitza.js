35bb713ab6feff840125d7b97cf5b20cb821ebcd
alexandrelan <alexandrelan@users.noreply.github.com>
2024-04-06 19:04:40 +0200

    tests

--- zuhaitza.js
+++ zuhaitza.js
@@ -1,17 +1,19 @@
-function zatitu(esaldia) {
-  return esaldia.trim().split(" ");
-}
-
-function sortuSuhaitza(esaldia) {
-    const suhaitza = {};
-    suhaitza.children = zatitu(esaldia);
-    return suhaitza;
-}
-
-function adieraziBerAdarreanDira(suhaitza, adabegia, besteadabegia) {
-
-}
-
-window.euskarikasi = {
-  sortuSuhaitza
-};
+(() => {
+  function zatitu(esaldia) {
+    return esaldia.trim().split(" ");
+  }
+  
+  function sortuSuhaitza(esaldia) {
+      const suhaitza = {};
+      suhaitza.children = zatitu(esaldia);
+      return suhaitza;
+  }
+  
+  function adieraziBerAdarreanDira(suhaitza, adabegia, besteadabegia) {
+  
+  }
+  
+  window.euskarikasi = {
+    sortuSuhaitza
+  };  
+})();
