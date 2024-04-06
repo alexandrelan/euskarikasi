e5931344a4c04f1db2f5bd59eb8207168b59af19
alexandrelan <alexandrelan@users.noreply.github.com>
2024-04-06 22:31:26 +0200

    test

--- zuhaitz.test.js
+++ zuhaitz.test.js
@@ -9,6 +9,11 @@ function error() {
 if (typeof sortuSuhaitza("test") !== "object") error();
 if (sortuSuhaitza("adio").emanAdarrak().length !== 1) error();
 if (sortuSuhaitza("ni naiz").emanAdarrak().length !== 2) error();
+if ((() => {
+  const suhaitza = sortuSuhaitza("ni etxe horretara noa");
+  suhaitza.elkartu(suhaitza.adarra(1), suhaitza.adarra(2));
+  return suhaitza.adarra(1).adarrak().length === 2;
+}) === false) error();
 
 document.addEventListener("DOMContentLoaded", () => {
   if (!window.euskarikasiError) document.body.innerHTML = "😍";
